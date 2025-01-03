c2d0701688c3bcc7e9f6037be20c1182d467aace
Taha Gad <taha.gad@solita.fi>
2025-01-03 10:38:56 +0200

    revised schedule configuration format

--- cloud_resources/azure/terraform/modules/function_files/trigger_functions.tf
+++ cloud_resources/azure/terraform/modules/function_files/trigger_functions.tf
@@ -8,20 +8,35 @@ locals {
       file => yamldecode(file("${path.root}/../../../config/${file}"))
   }
 
-  # Construct list of source systems with cron schedules
+  # Construct list of schedules
+  schedules = flatten([
+    for config in local.parsed_configs : [
+      for schedule in try(config.schedules, []) :
+        {
+          schedule_id = schedule.schedule_id
+          name = schedule.name
+          cron = schedule.cron
+        }
+        if try(schedule.schedule_id, null) != null
+        && length(try(schedule.name, "")) > 0
+        && length(try(schedule.cron, "")) > 0
+    ]
+  ])
+
+  # Construct list of source systems assigned schedules
   source_system_schedules = flatten([
     for config in local.parsed_configs : [
       for source in try(config.source_systems, []) :
         {
           ade_source_system = source.ade_source_system
           ade_source_entity = ""
-          cron_schedule = source.cron_schedule
+          schedule_id = source.schedule_id
         }
-        if length(try(source.cron_schedule, "")) > 0
+        if try(source.schedule_id, null) != null
     ]
   ])
 
-  # Construct list of source entities with cron schedules
+  # Construct list of source entities with assigned schedules
   source_entity_schedules = flatten([
     for config in local.parsed_configs : [
       for source in try(config.source_systems, []) : [
@@ -29,50 +44,43 @@ locals {
           {
             ade_source_system = source.ade_source_system
             ade_source_entity = entity.ade_source_entity
-            cron_schedule = entity.cron_schedule
+            schedule_id = entity.schedule_id
           }
-          if length(try(entity.cron_schedule, "")) > 0
+          if try(entity.schedule_id, null) != null
       ]
     ]
   ])
 
   # Combined list
-  schedules = setunion(local.source_system_schedules, local.source_entity_schedules)
+  assigned_schedules = setunion(local.source_system_schedules, local.source_entity_schedules)
 
-  # Distinct cron schedules
-  distinct_cron_schedules = distinct([
+  # Triggers
+  triggers = [
     for schedule in local.schedules :
-      schedule.cron_schedule
-  ]) 
-
-  # Triggers by cron_schedule
-  triggers_by_cron = [
-    for schedule in local.distinct_cron_schedules :
     {
-      cron_schedule = schedule
+      schedule_id = schedule.schedule_id
+      name = schedule.name
+      cron = schedule.cron
       triggers = [
-        for item in local.schedules :
+        for item in local.assigned_schedules :
           [      
             item.ade_source_system,
             item.ade_source_entity
           ]
-          if item.cron_schedule == schedule
+          if item.schedule_id == schedule.schedule_id
       ]
     }
   ]
-}
-
-output "test" {
-  value = local.triggers_by_cron
 }
 
 module "timer_trigger_function" {
   source = "../timer_trigger_function"
   function_output_folder = local.output_folder
   for_each = {
-    for cron in local.triggers_by_cron : cron.cron_schedule => cron
+    for index, trigger in local.triggers :
+      trigger.name => trigger
   }
-  function_name = "timer_${replace(replace(replace(each.key, " ", "_"), "/", ""), "*", "a")}" # Should probably use something else than cron as function name
-  cron_expression = each.key
+  function_name = "timer_${each.value.name}"
+  cron_expression = each.value.cron
   trigger_message = jsonencode({calls = each.value.triggers})
 }
