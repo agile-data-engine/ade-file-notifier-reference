9df2cac993e8412eb7695332fb98591fc1ff8162
Taha Gad <taha.gad@solita.fi>
2025-01-03 12:56:19 +0200

    queue message base64 encoded

--- cloud_resources/azure/terraform/modules/timer_trigger_function/function_files.tf
+++ cloud_resources/azure/terraform/modules/timer_trigger_function/function_files.tf
@@ -5,6 +5,7 @@ resource "local_file" "timer_trigger_py" {
     import azure.functions as func
     import os
     import json
+    import base64
     from azure.storage.queue import QueueClient
     from azure.identity import DefaultAzureCredential
 
@@ -13,13 +14,14 @@ resource "local_file" "timer_trigger_py" {
         queue_name = os.getenv('notify_queue')
         message_data = ${var.trigger_message}
         message_json = json.dumps(message_data)
+        message_base64 = base64.b64encode(message_json.encode('utf-8')).decode('utf-8')
 
         try:
             # Using managed identity
             credential = DefaultAzureCredential()
             queue_client = QueueClient(queue_url, queue_name=queue_name, credential=credential)
 
-            queue_client.send_message(message_json)
+            queue_client.send_message(message_base64)
             logging.info(f"Message added to queue: {message_json}")
 
         except Exception as e:
