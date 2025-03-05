af792518f89e9d5b92905f75b2b814707d5da404
Taha Gad <taha.gad@solita.fi>
2025-03-05 15:51:33 +0200

    check files for changes

--- cloud_resources/azure/terraform/modules/function_files/config.tf
+++ cloud_resources/azure/terraform/modules/function_files/config.tf
@@ -5,4 +5,5 @@ resource "azurerm_storage_blob" "config-files" {
   storage_container_name = var.storage_container_name
   type                   = "Block"
   source                 = "${path.root}/../../../${var.config_folder}/${each.key}"
+  content_md5            = filemd5("${path.root}/../../../${var.config_folder}/${each.key}")
 }
