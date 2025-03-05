7e11bca534e6fe2a8e5776ef6406c04ea7b7b059
Taha Gad <taha.gad@solita.fi>
2025-03-05 13:15:35 +0200

    handling single_file_manifest

--- functions/azure/queue_file/queue_file.py
+++ functions/azure/queue_file/queue_file.py
@@ -2,6 +2,9 @@ import azure.functions as func
 import logging
 import os
 import json
+import base64
+from azure.storage.queue import QueueClient
+from azure.identity import DefaultAzureCredential
 from shared.azure_handler import (
     AzureFileHandler,
     download_config
@@ -48,6 +51,11 @@ def queue_file(msg: func.QueueMessage):
 
             if upload_result:
                 logging.info(f"Uploaded file to Azure Blob Storage: {file_path}")
+
+                # If single file manifest option is True, notification will be done right away
+                # this is done by sending data to queue, which invokes the notification process
+                if source['attributes'].get('single_file_manifest', False):
+                    handle_single_file_manifest(source)
             else:
                 logging.error(f"Failed to upload {file_path} to Azure Blob Storage.")
                 return
@@ -55,3 +63,45 @@ def queue_file(msg: func.QueueMessage):
     except Exception as e:
         logging.error(f"Error processing event: {event_data}")
         raise
+
+def handle_single_file_manifest(source):
+    """
+    Sends trigger message to notify queue when 'single_file_manifest' is True.
+    
+    Args:
+        source: The source dictionary containing attributes.
+    
+    Raises:
+        ValueError: If required attributes are missing or empty.
+
+    Returns:
+        None
+    """
+    
+    # Validate required attributes
+    source_system = source['attributes'].get('ade_source_system')
+    source_entity = source['attributes'].get('ade_source_entity')
+
+    if not source_system or not source_entity:
+        logging.error("Required attributes 'ade_source_system' or 'ade_source_entity' are missing or empty.")
+        raise ValueError("Both 'ade_source_system' and 'ade_source_entity' must be provided and non-empty.")
+    
+    # Construct the message
+    message_data = {
+        "calls": [[source_system, source_entity]]
+    }
+    message_json = json.dumps(message_data)
+    message_base64 = base64.b64encode(message_json.encode('utf-8')).decode('utf-8')
+    queue_url = os.getenv('AzureWebJobsStorage__queueServiceUri')
+    queue_name = os.getenv('notify_queue')
+
+    try:
+        # Using managed identity
+        credential = DefaultAzureCredential()
+        queue_client = QueueClient(queue_url, queue_name=queue_name, credential=credential)
+
+        queue_client.send_message(message_base64)
+        logging.info(f"Message added to queue: {message_json}")
+
+    except Exception as e:
+        logging.error(f"Error adding message to queue: {e}")
