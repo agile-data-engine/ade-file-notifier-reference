2797cebaca49b12cadf8b806c9968a63ebddf845
Henri Hemminki <henri.hemminki@solita.fi>
2026-02-19 09:02:59 +0200

    enabling rbac

--- cloud_resources/azure/terraform/modules/secrets/access_policies.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_role_assignment" "kv_secrets_officer" {
-    for_each             = toset(var.security_group_ids)
-    scope                = azurerm_key_vault.notifier.id
-    role_definition_name = "Key Vault Secrets Officer"
-    principal_id         = each.value
-}
